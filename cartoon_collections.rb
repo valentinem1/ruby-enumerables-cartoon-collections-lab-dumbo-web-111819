a58208a12c01a182c9cb2542fb4df366fc9fe45f
Valentine Maillard <valentinemaillard1@gmail.com>
2019-11-03 21:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each_with_index(){|item, index|
+  puts "#{index + 1} #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)
   # Your code here
+  veggies.map {|word| word.capitalize << "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls_long)
   # Your code here
+  calls_long.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(contains_cheddar)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < cheese_types.length do
+    puts cheese_types[i]
+end
+ contains_cheddar.include?(cheese_types[i])
 end
